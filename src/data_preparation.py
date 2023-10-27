8b6cb7b0611efe53a20af74a5f586dc6aa212ecf
Samuel Launay-Pariente <samuel.launay@hotmail.fr>
2023-10-27 09:47:03 +0200

    new, MAE : 3.12

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -11,26 +11,11 @@ warnings.simplefilter("ignore", category=ConvergenceWarning)
 
 
 class DataPreparation:
-    """
-    DataPreparation class to prepare the data
-    """
-
     def __init__(self, train, test):
         self.train = train.copy()
         self.test = test.copy()
 
     def get_variable_correlation(self, variable):
-        """
-        Get the correlation vector for a given variable.
-
-        Parameters:
-            variable (str): The name of the variable to calculate
-            the correlation vector for.
-
-        Returns:
-            pd.Series: The correlation vector for the given variable,
-            sorted in descending order.
-        """
         correlation_vector = self.train[self.col_numericals].corr()[variable][:]
         correlation_vector = np.abs(correlation_vector)
         correlation_vector = correlation_vector.sort_values(ascending=False)[1:]
@@ -38,20 +23,6 @@ class DataPreparation:
         return correlation_vector
 
     def get_nan_table(self):
-        """
-        Generates a table showing the percentage of missing values for each
-        variable in the dataset.
-
-        Returns:
-            pandas.DataFrame: A DataFrame containing three columns: "Variable",
-            "Pourcentages de valeurs manquantes", and "Type".
-                - The "Variable" column contains the names of the variables
-                in the dataset.
-                - The "Pourcentages de valeurs manquantes" column contains
-                the percentage of missing values for each variable.
-                - The "Type" column indicates whether the variable
-                is numerical or categorical.
-        """
         nan_table = pd.DataFrame(
             columns=["Variable", "Pourcentages de valeurs manquantes", "Type"]
         )
@@ -68,79 +39,42 @@ class DataPreparation:
         return nan_table
 
     def remove_train_nan(self):
-        """
-        Removes columns with a high percentage of missing values from
-        the train dataset.
-
-        Returns:
-            None
-        """
         percentage = (self.train.isna().sum() / self.train.shape[0]) * 100
-
         self.columns_to_delete = [
             col
             for col in self.train.columns
             if col != "Electric range (km)" and percentage[col] >= 40
         ]
-        self.columns_to_delete.extend(["r", "Status", "IT", "Mp"])
+        self.columns_to_delete.extend(
+            ["r", "Status", "IT", "Date of registration", "Mp"]
+        )
         self.train.drop(columns=self.columns_to_delete, inplace=True)
         print("Valeurs manquantes du train supprimées ✅")
 
     def remove_test_nan(self):
-        """
-        Remove nan values from the test dataframe.
-        """
         self.test.drop(columns=self.columns_to_delete, inplace=True)
         print("Valeurs manquantes du test supprimées ✅")
 
-    def remove_train_test_outliers(self):
-        """
-        Remove outliers from the train and test datasets based on the "W (mm)"
-        column.
-        """
-        self.train = self.train.loc[self.train["W (mm)"] >= 1500]
-        self.test = self.test.loc[self.test["W (mm)"] >= 1500]
-        print("Outliers traités ✅")
+    def rename_columns(self):
+        self.train.columns = self.train.columns.str.replace(" ", "_")
+        self.test.columns = self.test.columns.str.replace(" ", "_")
+        print("Variables renommées ✅")
 
     def get_type_list(self):
-        """
-        Get the list of column types in the dataset.
-
-        This function retrieves the list of column types in the dataset
-        and stores them in
-        class variables `col_categoricals` and `col_numericals`.
-
-        Parameters:
-        - self: The instance of the class.
-
-        Returns:
-        None
-        """
         self.col_categoricals = self.train.select_dtypes(
             include=["object"]
         ).columns.to_list()
         self.col_numericals = self.train.select_dtypes(
             exclude=["object"]
         ).columns.to_list()
-        self.train["date"] = pd.to_datetime(self.train["Date of registration"])
         self.col_numericals.remove("ID")
 
     def impute_train_test_numerical(self):
-        """
-        Imputes missing values in the train and test datasets for
-        numerical features.
-
-        Parameters:
-        - None
-
-        Returns:
-        - None
-        """
         # Imputation of all numerical features except Eletric Range and Fuel Consumption
         var_to_impute = self.col_numericals.copy()
-        var_to_impute.remove("Electric range (km)")
-        var_to_impute.remove("Ewltp (g/km)")
-        var_to_impute.remove("Fuel consumption ")
+        var_to_impute.remove("Electric_range_(km)")
+        var_to_impute.remove("Ewltp_(g/km)")
+        var_to_impute.remove("Fuel_consumption_")
 
         X_train, X_test = self.train[var_to_impute], self.test[var_to_impute]
 
@@ -154,49 +88,40 @@ class DataPreparation:
         self.test.loc[:, var_to_impute] = X_test_imputed
 
         # Imputation of Electric range
-        self.train["Electric range (km)"].fillna(0, inplace=True)
-        self.test["Electric range (km)"].fillna(0, inplace=True)
+        self.train["Electric_range_(km)"].fillna(0, inplace=True)
+        self.test["Electric_range_(km)"].fillna(0, inplace=True)
 
         # Imputation of Fuel Consumption
         var_explicatives = self.get_variable_correlation(
-            "Fuel consumption "
+            "Fuel_consumption_"
         ).index.to_list()
-        var_explicatives.remove("Ewltp (g/km)")
+        var_explicatives.remove("Ewltp_(g/km)")
         var_explicatives = var_explicatives[:3]
 
         var_full = var_explicatives.copy()
-        var_full.append("Fuel consumption ")
+        var_full.append("Fuel_consumption_")
 
         df_train = self.train[var_full].dropna(how="any")
-        index_NAN_train = self.train[self.train["Fuel consumption "].isna()].index
-        index_NAN_test = self.test[self.test["Fuel consumption "].isna()].index
+        index_NAN_train = self.train[self.train["Fuel_consumption_"].isna()].index
+        index_NAN_test = self.test[self.test["Fuel_consumption_"].isna()].index
 
         reg = LinearRegression().fit(
-            df_train[var_explicatives], df_train["Fuel consumption "]
+            df_train[var_explicatives], df_train["Fuel_consumption_"]
         )
 
         pred_train = reg.predict(self.train.loc[index_NAN_train, var_explicatives])
         pred_test = reg.predict(self.test.loc[index_NAN_test, var_explicatives])
 
-        self.train.loc[index_NAN_train, "Fuel consumption "] = pred_train
-        self.test.loc[index_NAN_test, "Fuel consumption "] = pred_test
+        self.train.loc[index_NAN_train, "Fuel_consumption_"] = pred_train
+        self.test.loc[index_NAN_test, "Fuel_consumption_"] = pred_test
 
-        self.train.loc[self.train["Fuel consumption "] <= 0, "Fuel consumption "] = 0
-        self.test.loc[self.test["Fuel consumption "] <= 0, "Fuel consumption "] = 0
+        self.train.loc[self.train["Fuel_consumption_"] <= 0, "Fuel_consumption_"] = 0
+        self.test.loc[self.test["Fuel_consumption_"] <= 0, "Fuel_consumption_"] = 0
 
         print("Valeurs manquantes numériques imputées ✅")
 
     def impute_train_test_categorical(self):
-        """
-        Imputes missing categorical values in the train and test datasets.
-
-        Parameters:
-            None
-
-        Returns:
-            None
-        """
-        # imputation 'Cn'
+        # Imputation of 'Cn'
         mode_VFN_train = self.train.groupby("T")["Cn"].apply(
             lambda x: x.mode().iloc[0] if not x.mode().empty else None
         )
@@ -212,7 +137,7 @@ class DataPreparation:
 
         self.test["Cn"].fillna(self.test["Cn"].mode()[0], inplace=True)
 
-        # Imputation 'VFN'
+        # Imputation of 'VFN'
         mode_VFN_train = self.train.groupby("Cn")["VFN"].apply(
             lambda x: x.mode().iloc[0] if not x.mode().empty else None
         )
@@ -245,10 +170,6 @@ class DataPreparation:
 
         self.test["VFN"].fillna(self.test["VFN"].mode()[0], inplace=True)
 
-        # drop de 'date of registration'
-        self.train.drop(columns="Date of registration", inplace=True)
-        self.test.drop(columns="Date of registration", inplace=True)
-
         # Imputation des variables ayant moins de 1% de NaN
         for col in ["Tan", "T", "Va", "Ve", "Mk", "Ct", "Fm"]:
             self.train[col].fillna(self.train[col].mode()[0], inplace=True)
@@ -257,24 +178,9 @@ class DataPreparation:
         print("Valeurs manquantes catégorielles imputées ✅")
 
     def prepare_data(self):
-        """
-        Prepare the data for training and testing.
-
-        This function performs the following operations:
-        - Removes NaN values from the training dataset.
-        - Removes NaN values from the testing dataset.
-        - Removes outliers from the training and testing datasets.
-        - Retrieves the list of data types in the dataset.
-        - Imputes missing numerical values in the train and test datasets.
-        - Imputes missing categorical values in the train and test datasets.
-
-        Returns:
-        - train (DataFrame): The preprocessed training dataset.
-        - test (DataFrame): The preprocessed testing dataset.
-        """
         self.remove_train_nan()
         self.remove_test_nan()
-        self.remove_train_test_outliers()
+        self.rename_columns()
         self.get_type_list()
         self.impute_train_test_numerical()
         self.impute_train_test_categorical()
